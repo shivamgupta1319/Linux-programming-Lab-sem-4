216868221e742bae8537645a1a65511091400172
Shivam Gupta <sg247938@gmail.com>
2021-06-15 14:58:47 +0530

    Add files via upload

---
+++ case_control.sh
@@ -0,0 +1,20 @@
+echo "enter days number"
+read d
+case $d in
+1) echo "sunday"
+;;
+2) echo "monday"
+;;
+3) echo "tuesday"
+;;
+4) echo "wednesday"
+;;
+5) echo "thursday"
+;;
+6) echo "friday"
+;;
+7) echo "saturday"
+;;
+*) echo "enter the correct day number like between 1 to 7"
+
+esac
